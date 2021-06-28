32a22d662f8d1e1f646ea7b9bb869b3c038f6e42
Ava <ava.gyx@gmail.com>
2021-06-28 04:23:08 -0700

    added valid moves checking conditions for knight and rook (in progress)

--- pieces.c
+++ pieces.c
@@ -5,16 +5,45 @@ struct MoveList {
     Coordinate[] coord;
 };
 
-struct MoveList get_valid_moves(struct Coordinate coord, char piece_type) {
+/**
+Parameters:
+    coord: coordinate of current piece
+    piece_type: current piece type
+Return:
+    moves: an array of all valid moves that current piece can make
+**/
+struct MoveList get_valid_moves(struct Coordinate coord, char piece_type, Board *b) {
     struct MoveList moves;
-    
+
     switch (piece_type) {
         case BKNIGHT:
         case WKNIGHT:
         {
             moves.coord = (Coordinate*) malloc(8 * sizeof(Coordinate));
-            moves.coord[0].r = coord.r -
-            moves.coord[0].c =
+
+            moves.coord[0].r = coord.r + 1
+            moves.coord[0].c = coord.c - 2
+
+            moves.coord[1].r = coord.r + 1
+            moves.coord[1].c = coord.c + 2
+
+            moves.coord[2].r = coord.r - 1
+            moves.coord[2].c = coord.c - 2
+
+            moves.coord[3].r = coord.r - 1
+            moves.coord[3].c = coord.c + 2
+
+            moves.coord[4].r = coord.r + 2
+            moves.coord[4].c = coord.c - 1
+
+            moves.coord[5].r = coord.r + 2
+            moves.coord[5].c = coord.c + 1
+
+            moves.coord[6].r = coord.r - 2
+            moves.coord[6].c = coord.c - 1
+
+            moves.coord[7].r = coord.r - 2
+            moves.coord[7].c = coord.c + 1
             break;
         }
         case BQUEEN:
@@ -38,7 +67,16 @@ struct MoveList get_valid_moves(struct Coordinate coord, char piece_type) {
         case BROOK:
         case WROOK:
         {
-            //todo
+            moves.coord = (Coordinate*) malloc(14 * sizeof(Coordinate));
+            int i = 0;
+            for (int r = coord.r + 1; r < 8; ++r) {
+                moves.coord[i].r = r;
+                moves.coord[i].c = coord.c;
+                ++i;
+                if (b.squares[r][coord.c] != EMPTY) {
+                    break;
+                }
+            }
             break;
         }
         case BPAWN:{
@@ -57,6 +95,6 @@ struct MoveList get_valid_moves(struct Coordinate coord, char piece_type) {
             moves.length = 0;
             moves.coord = Null;
     }
-    
+
     return moves;
 }
