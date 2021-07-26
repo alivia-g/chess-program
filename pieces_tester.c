519c0e6d3a40a515a4be8822ef1df38ffb30ea6b
Ava <ava.gyx@gmail.com>
2021-07-25 21:02:03 -0700

    implemented for loop to print out valid moves

--- pieces_tester.c
+++ pieces_tester.c
@@ -34,6 +34,11 @@ int main() {
         struct Coordinate coord = algebraic_to_coordinate(pos);
         struct MoveList valid_moves = get_valid_moves(coord, piece_type, &b);
         printf("Number of valid moves: %d\n", valid_moves.length);
+        printf("Valid moves are:\n");
+        for (int i = 0; i < valid_moves.length; ++i) {
+            //place_piece(piece_type, valid_moves.coord[i], &b);
+            printf("%d,%d\n", valid_moves.coord[i].r, valid_moves.coord[i].c);
+        }
     }
     return 0;
 }
